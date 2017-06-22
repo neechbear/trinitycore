051940916680985bafc752c52c9ab1b30f2816c5
Nicola Worthington <nicolaw@tfb.net>
2017-06-22 23:00:19 +0100

    c&p fail

--- build.sh
+++ build.sh
@@ -6,74 +6,15 @@ log_notice() {
   echo -e "\033[0;1;33m$*\033[0m"
 }
 
-log_info() {
-  echo -e "\033[0;1m$*\033[0m"
-}
-
-get_tdb_url() {
-  declare tag="${1:-TDB}"
-  # https://developer.github.com/v3/
-  # https://github.com/TrinityCore/TrinityCore/releases
-  curl -sSL ${GITHUB_USER:+"-u$GITHUB_USER:$GITHUB_PASS"} \
-    "${GITHUB_API}/repos/${GITHUB_REPO}/releases" \
-    | jq -r "\"tdb_url=\" + ( [
-                .[] | select(
-                .tag_name | contains( \"$tag\" ) )
-                .assets[] .browser_download_url
-              ] | max )"
-}
-
-download_source() {
-  declare target="${1%/}"
-  declare branch="$2"
-  declare tdb_tag="TDB${branch//[^0-9]/}"
-  declare repo_url="https://github.com/${GITHUB_REPO%%.git}.git"
-
-  log_notice "Fetchhing $branch ($tdb_tag) ..."
-
-  if [[ -r "$target/database.url" ]]; then
-    declare tdb_url="$(< "$target/database.url")"
-  fi
-  if [[ -z "${tdb_url:-}" ]] ; then
-    declare $(get_tdb_url "$tdb_tag")
-    echo "$tdb_url" > "$target/database.url"
-  fi
-  log_info " -> $repo_url ($branch branch)"
-  log_info " -> $tdb_url"
-
-  if [[ ! -e "$target/trinitycore" ]]; then
-    git clone -b "$branch" --depth 1 \
-      "$repo_url" "$target/trinitycore"
-  else
-    git -C "$target/trinitycore" pull
-  fi
-
-  if [[ ! -s "$target/${tdb_url##*/}" ]]; then
-    echo "Downloading database $tdb_url ..."
-    curl -L --progress-bar -o "$target/${tdb_url##*/}" "$tdb_url"
-  fi
-}
-
 main() {
-  declare GITHUB_USER="${GITHUB_USER:-}"
-  declare GITHUB_PASS="${GITHUB_PASS:-}"
-  declare GITHUB_API="https://api.github.com"
-  declare GITHUB_REPO="TrinityCore/TrinityCore"
-
   declare version="${1:-3.3.5}"
-  declare source="${BASH_SOURCE[0]%/*}/build/$version"
   declare artifacts="${BASH_SOURCE[0]%/*}/artifacts/$version"
 
-  mkdir -p "$source" "$artifacts"
-
-  download_source "$source" "$version"
-
   log_notice "Building Docker build container ..."
-  cp docker/build/* "$source"
-  docker build -t "trinitycore/build:$version" "$source"
-  docker images "trinitycore/build:$version" | tee "$source/images.txt"
+  docker build -t "trinitycore/build:$version" docker/build
 
   log_notice "Building TrinityCore ..."
+  mkdir -p "$artifacts"
   docker run -it --rm -v "$(readlink -f "$artifacts")":/build "trinitycore/build:$version"
 }
 
--- docker/build/build.sh
+++ docker/build/build.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
+if ! source /usr/lib/blip.bash ; then
+  >&2 echo "Missing dependency 'blip' (https://nicolaw.uk/blip); exiting!"
+  exit 2
+fi
+
+_parse_command_line_arguments () {
+  cmdarg_info "header" "TrinityCore Dockerised build wrapper."
+  cmdarg_info "version" "1.0"
+
+  cmdarg_info "author" "Nicola Worthington <nicolaw@tfb.net>."
+  cmdarg_info "copyright" "(C) 2017 Nicola Worthington."
+
+  cmdarg_info "footer" \
+    "See https://github.com/neechbear/trinitycore, https://neech.me.uk and"
+    "https://nicolaw.uk/#WoW."
+
+  cmdarg 'b'  'branch'  'Branch (version) of TrinityCore to build' '3.3.5'
+  cmdarg 'r'  'repo'    'Git repository to clone from' 'https://github.com/TrinityCore/TrinityCore.git'
+  cmdarg 'd?' 'tdb'     'TDB database archive URL to use'
+  cmdarg 'v'  'verbose' 'Print more verbose debugging output'
+
+  cmdarg_parse "$@" || return $?
+}
 
 get_tdb_url() {
   declare tag="${1:-TDB}"
   # https://developer.github.com/v3/
   # https://github.com/TrinityCore/TrinityCore/releases
   curl -sSL ${GITHUB_USER:+"-u$GITHUB_USER:$GITHUB_PASS"} \
-    "${GITHUB_API}/repos/${GITHUB_REPO}/releases" \
+    "${GITHUB_API:-https://api.github.com}/repos/${GITHUB_REPO:-TrinityCore/TrinityCore}/releases" \
     | jq -r "\"tdb_url=\" + ( [
                 .[] | select(
                 .tag_name | contains( \"$tag\" ) )
@@ -18,18 +41,24 @@ get_tdb_url() {
 download_source() {
   declare target="${1%/}"
   declare branch="$2"
-  declare tdb_tag="TDB${branch//[^0-9]/}"
+  declare tdb_url="${3:-}"
+
   declare repo_url="https://github.com/${GITHUB_REPO%%.git}.git"
 
-  log_notice "Fetchhing $branch ($tdb_tag) ..."
-
-  if [[ -r "$target/database.url" ]]; then
-    declare tdb_url="$(< "$target/database.url")"
+  # Determine what TDB database archive URL to download.
+  if ! is_url "$tdb_url"; then
+    if [[ "$tdb_url" =~ "TDB"* ]]; then
+      tdb_tag="$tdb_url"
+    else
+      tdb_tag="${branch//[^0-9]/}"
+    fi
   fi
   if [[ -z "${tdb_url:-}" ]] ; then
     declare $(get_tdb_url "$tdb_tag")
     echo "$tdb_url" > "$target/database.url"
   fi
+
+  log_notice "Fetchhing $branch ($tdb_tag) ..."
   log_info " -> $repo_url ($branch branch)"
   log_info " -> $tdb_url"
 
@@ -47,24 +76,19 @@ download_source() {
 }
 
 main() {
-  declare cpus="$(nproc)"
+  declare -gA cmdarg_cfg=()
+  _parse_command_line_arguments "$@" || exit $?
 
-  # If you are rate-limited, pass GITHUB_USER and GITHUB_PASS to the container.
-  # Authenticated API calls have a much higher query quota.
-  declare GITHUB_USER="${GITHUB_USER:-}"
-  declare GITHUB_PASS="${GITHUB_PASS:-}"
-  declare GITHUB_API="https://api.github.com"
-  declare GITHUB_REPO="TrinityCore/TrinityCore"
+  if [[ -n "${cmdarg_cfg[verbose]}" || -n "${DEBUG:-}" ]] ; then
+    for i in "${!cmdarg_cfg[@]}" ; do
+      printf '${cmdarg_cfg[%s]}=%q\n' "$i" "${cmdarg_cfg[$i]}"
+    done
+  fi
 
-  declare version="${1:-3.3.5}"
-  declare source="${BASH_SOURCE[0]%/*}/build/$version"
-  declare artifacts="${BASH_SOURCE[0]%/*}/artifacts/$version"
+  declare source="/usr/local/src/${cmdarg_cfg[branch]}"
+  mkdir -p "$source"
 
-  mkdir -p "$source" "$artifacts"
-
-  download_source "$source" "$version"
-  mkdir build
-  cd build
+  download_source "$source" "${cmdarg_cfg[branch]}"
 
   # https://askubuntu.com/questions/828989/cmake-cant-find-boost
   declare boost_opts=""
@@ -78,6 +102,10 @@ main() {
   done
 
   # https://trinitycore.atlassian.net/wiki/display/tc/Linux+Core+Installation
+  mkdir -p "$source/trinitycore/build"
+  cd "$source/trinitycore/build"
+
+  declare cpus="$(nproc)"
   cmake ../ -DPREFIX=/artifacts -DTOOLS=1 -DWITH_WARNINGS=0 -Wno-dev ${boost_opts}
   make -j "${cpus:-1}"
   make install
