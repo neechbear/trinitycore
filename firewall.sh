7cf88927a1514d99a59496d79b0b94a0d57ae774
Nicola Worthington <nicolaw@tfb.net>
2017-06-25 22:48:41 +0100

    Herping the derp derp (silly scoping error)

---
+++ firewall.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -euo pipefail
+
+ufw_applications() {
+  cat <<EOM
+TrinityCore AuthServer
+TrinityCore BattleNet
+TrinityCore WorldServer
+TrinityCore WorldServer RA
+TrinityCore WorldServer SOAP
+EOM
+}
+
+my_networks() {
+  cat "${BASH_SOURCE[0]%/*}/.networks"
+}
+
+main() {
+  declare app="" net=""
+  while read -r net ; do
+    while read -r app ; do
+      ufw allow from "$net" to any app "$app"
+    done < <(ufw_applications)
+  done < <(my_networks)
+}
+
+main "$@"
