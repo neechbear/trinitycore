6927bcddee723e1af4e7185c6223368cb53acd30
Nicola Worthington <nicolaw@tfb.net>
2017-06-25 23:46:17 +0100

    dirty hack, have a better idea ?

--- fpm-mapdata.sh
+++ fpm-mapdata.sh
@@ -10,21 +10,25 @@ package() {
   declare type="${2:-deb}"
 
   # https://fpm.readthedocs.io/en/latest/source/dir.html
+  set -xv
+
   fpm \
     --input-type dir \
     --output-type deb \
     --name trinitycore-mapdata --version 3.3.5 \
     --verbose \
-    --url="https://www.trinitycore.org" \ 
+    --url "https://www.trinitycore.org" \
     --category "Amusements/Games" \
     --vendor "TrinityCore" \
     --directories "/opt/trinitycore/$source" \
     "$source"=/opt/trinitycore
+
+  { set +xv; } >/dev/null 2>&1
 }
 
 main() {
-  pushd "${BASH_SOURCE[0]%/*}/artifacts/mapdata"
-  package "dbc" "deb"
+  pushd "${BASH_SOURCE[0]%/*}/artifacts"
+  package "mapdata" "deb"
   popd
 }
 
